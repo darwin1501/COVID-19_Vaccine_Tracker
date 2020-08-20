51cf25b8e977f401909c803ab57c1e405cb2637f
darwin1501 <darwin.pablo1501@gmail.com>
2020-08-20 15:02:22 +0800

    added total vaccine and phase list

--- js/logic.js
+++ js/logic.js
@@ -26,6 +26,59 @@ const getVaccineInfo = (()=>{
 
 	const vaccine = JSON.parse( sessionStorage.vaccine );
 
-	console.log(vaccine)
+	const totalCandidates = vaccine.totalCandidates;
+
+	const vaccinePhases = vaccine.phases;
+
+	const totalVacTxt =  document.getElementById('vaccine-data');
+
+	totalVacTxt.innerHTML = totalCandidates;
+
+	console.log(vaccine);
+
+	for (phases = 0; phases < vaccinePhases.length; phases++) {
+		// console.log(vaccinePhases[phases]);
+
+		const numOfCandidates = vaccinePhases[phases].candidates;
+
+		const phaseList = vaccinePhases[phases].phase;
+
+		//select parent element
+		const vaccineContainer = document.getElementById('vaccine-phases');
+
+		// create element
+		const thinCard = document.createElement('div');
+
+		const thinContent = document.createElement('div');
+
+		const phaseTxt = document.createElement('h3');
+
+		const phaseNum = document.createElement('h3');
+
+		thinCard.setAttribute('class', 'thin-card');
+
+		thinContent.setAttribute('class', 'thin-content');
+
+		phaseTxt.setAttribute('class', 'phase-txt');
+
+		phaseTxt.setAttribute('id', 'phase-txt');
+
+		phaseTxt.innerHTML = `${phaseList}: `;
+
+		phaseNum.setAttribute('class', 'phase-num');
+
+		phaseNum.setAttribute('id', 'phase-num');
+
+		phaseNum.innerHTML = numOfCandidates;
+
+		thinContent.appendChild(phaseTxt);
+
+		thinContent.appendChild(phaseNum);
+
+		thinCard.appendChild(thinContent);
+
+		vaccineContainer.appendChild(thinCard);
+	};
+
 
 })
