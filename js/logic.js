41327d8e857298383825338b77bbb61456f151e7
darwin1501 <darwin.pablo1501@gmail.com>
2020-08-26 11:25:36 +0800

    added card flipping and its content

--- js/logic.js
+++ js/logic.js
@@ -36,6 +36,7 @@ const getVaccineInfo = (()=>{
 
 	console.log(vaccine);
 
+	//vaccine phases
 	for (phases = 0; phases < vaccinePhases.length; phases++) {
 		// console.log(vaccinePhases[phases]);
 
@@ -80,5 +81,70 @@ const getVaccineInfo = (()=>{
 		vaccineContainer.appendChild(thinCard);
 	};
 
+	//each vaccine info.
+	for (let vaccineData = 0; vaccineData < vaccine.data.length; vaccineData++) {
+	
+		const vaccineInfoTemplate =`
+		<div class="scene scene--card">
+			  <div class="card-flipping">
+			    <div class="card__face card__face--front">
+			    	<div class="front-container">
+			    		<p class="front-txt">${vaccine.data[vaccineData].candidate}</p>
+			    		<br>
+			    		<p class="sub-front-txt">Trial Phase: ${vaccine.data[vaccineData].trialPhase}</p>
+			    	</div>
+			    </div>
+			    <div class="card__face card__face--back">
+			    	<div class="back-container">
+			    		<!-- template string -->
+				    <p class="back-txt">
+				    	<label>Candidate: Lorem ipsum dolor sit.</label>
+				    	<br>
+				    	<br>
+				    	Sponsors: Lorem ipsum dolor sit.
+				    	<br>
+				    	<br>
+					    Details: Lorem ipsum dolor sit amet consectetur adipisicing elit. Error quas perspiciatis modi nobis at distinctio! At aut alias accusantium voluptas tenetur. Veniam possimus minus animi, itaque quo, vero dolor nam!
+					    <br>
+					    <br>
+					    Trial Phase: Early research.
+					    <br>
+					    <br>
+					    Institutions: Multiple study sites in Europe and North America
+					    <br>
+					    <br>
+					    Funding: perspiciatis modi
+					</p>
+					</div>
+			   </div>
+			  </div>
+			</div>`
+
+			const vaccineList = document.getElementById('vaccine-list');
+
+	// vaccineList.innerHTML = vaccineInfoTemplate;
+	// let doc = new DOMParser().parseFromString(vaccineInfoTemplate, 'text/html');
+
+	// console.log(doc);
+
+	// console.log(vaccineList)
+
+	vaccineList.insertAdjacentHTML('beforeend', vaccineInfoTemplate)
+
+	};
+
+	const cardFlipping = document.getElementsByClassName('card-flipping');
+
+	// console.log(cardFlipping.length);
+
+	for(let element = 0; element < cardFlipping.length; element++){
+		// console.log(cardFlipping[element]);
+
+		cardFlipping[element].addEventListener( 'click', function() {
+	  		cardFlipping[element].classList.toggle('is-flipped');
+		});
+}
+
+// console.log(cardFlipping.lenght);
 
 })
