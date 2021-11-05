7094428a758522aeba3bdfea267cc111633e09f2
darwin1501 <darwin.pablo1501@gmail.com>
2021-11-05 18:33:43 +0800

    remove pie chart

--- js/data_chart.js
+++ js/data_chart.js
@@ -1,28 +1,28 @@
 
-const ctx = document.getElementById('pieChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ['Red', 'Blue'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+// const pieChart = document.getElementById('pieChart').getContext('2d');
+// const pieChartConfig = new Chart(pieChart, {
+//     type: 'pie',
+//     data: {
+//         labels: ['Unvaccinated', 'Vaccinated'],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//             ],
+//             borderColor: [
+//                 'rgba(255, 99, 132, 1)',
+//                 'rgba(54, 162, 235, 1)',
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
